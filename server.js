7840c65885a1175ebd3319f03091486fdd13dc5e
keanedawg <cfife@chicagoventure.com>
2017-05-22 12:35:58 -0500

    catching branch errors successfully

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
-var Git;
+var Git = require("nodegit");
 
-console.log("hello world!");
+var getMostRecentCommit = function(repository) {
+  return repository.getBranchCommit("master");
+};
 
+var makeBranch = function(respository) {
+  console.log(repository)
+  repository.createBranch("tester", getMostRecentCommit(repository)).then(
+      function(reference) {console.log("good")}, 
+      function(reasonForFailure) {console.log("bad1")}).catch(
+      function(reasonForFailure) {console.log("bad2")});
+};
+
+var getCommitMessage = function(commit) {
+  return commit.message();
+};
+
+Git.Repository.open("../test_repo")
+  .then(getMostRecentCommit).catch(
+      function(reasonForFailure) {console.log(reasonForFailure)});
